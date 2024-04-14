3713cbf1681f8f1cf156d5b1508b0c3dccec9db6
Anshul <anshul3071@gmail.com>
2024-04-14 16:10:52 +0530

    The development of version 1 has been completed, and all features have been added such as onclick resume download, let's chat

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function scrollToChat(){
-    var element = document.getElementById('lets-chat');
-    element.scrollIntoView({behavior: 'smooth'});
+// Function to set scroll to particular div
+function scrollToChat() {
+  var element = document.getElementById("lets-chat");
+  element.scrollIntoView({ behavior: "smooth" });
 }
+
+// to download
+document.querySelector(".resume-download").addEventListener("click", function(){
+    let result = window.confirm("Do you want to download the Resume ?");
+    if(result === false)
+        event.preventDefault();
+});
